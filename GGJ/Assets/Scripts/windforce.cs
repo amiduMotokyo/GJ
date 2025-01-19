c2abca3d5b5d3d527e0dbce3ef3cce8566c1c306
UNFOF <161683046+UNFOF@users.noreply.github.com>
2025-01-19 23:06:34 +0800

    风压实现

--- GGJ/Assets/Scripts/windforce.cs
+++ GGJ/Assets/Scripts/windforce.cs
@@ -10,15 +10,40 @@ public class windforce : MonoBehaviour
     public float Windforce;
     [Header("方向")]
     public windDirection Direction;
+    private Vector2 _winddir;
 
+    private void Awake()
+    {
+        changedir(Direction);
+    }
+
+
+
+    private void Update()
+    {
+        addforce();
+    }
+
+    void changedir(windDirection dir)
+    {
+        switch (dir)
+        {
+            case windDirection.右: transform.localEulerAngles = new Vector3(0, 0, 0); _winddir = Vector2.right ; break;
+            case windDirection.左: transform.localEulerAngles = new Vector3(0, 180, 0); _winddir = Vector2.left; break;
+            case windDirection.下: transform.localEulerAngles = new Vector3(0, 0, -90); _winddir = Vector2.down; break;
+            case windDirection.上: transform.localEulerAngles = new Vector3(0, 0, 90); _winddir = Vector2.up; break;
+        }
+    }
 
     void addforce()
     {
         if (_bubble != null)
         {
-            for(var i=0; i < _bubble.Count;)
+            for(var i=0; i < _bubble.Count;i++)
             {
-                //_bubble[i].GetComponent<Rigidbody2D>().AddForce();
+                Debug.Log("move");
+                var rb = _bubble[i].GetComponent<Rigidbody2D>();
+                rb.AddForce(_winddir);
             }
         }
     }
@@ -28,7 +53,15 @@ public class windforce : MonoBehaviour
     {
         if (collision.CompareTag("bubble"))
         {
-            _bubble.Add(collision.gameObject);
+            Debug.Log("get");
+            if (!_bubble.Contains(collision.gameObject))
+            {
+                _bubble.Add(collision.gameObject);
+            }
+            else
+            {
+                Debug.Log("exist");
+            }
         }
     }
 }
